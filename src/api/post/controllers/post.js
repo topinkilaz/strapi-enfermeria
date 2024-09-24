333d29e1046da0032ce4102003102c572ce0b215
topinkilaz <juan.p.t.1234@gmail.com>
2024-09-24 16:21:54 -0400

    controller failed

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -6,9 +6,8 @@ module.exports = factories.createCoreController('api::post.post', ({ strapi }) =
   async find(ctx) {
     const posts = await strapi.db.query('api::post.post').findMany({
       orderBy: { id: 'asc' },
-      // Incluir las relaciones necesarias
       populate: {
-        image: true, // Cambia 'image' por el nombre real de tu campo de imagen
+        image: true,
       },
     });
 
@@ -23,7 +22,7 @@ module.exports = factories.createCoreController('api::post.post', ({ strapi }) =
     const post = await strapi.db.query('api::post.post').findOne({
       where: { id: parseInt(id) },
       populate: {
-        image: true, // Cambia 'image' por el nombre real de tu campo de imagen
+        image: true,
       },
     });
 
